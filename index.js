4d5f9a577b41fdf5aa376772f1ce7d7b93a6392c
John Hayes <johnnyhayes7@gmail.com>
2018-09-17 19:01:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array=[];
+  for (var i=0; i<4; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+   return array;
+}
+
+function johnLennonFacts(array){
+  var n=0;
+  var lennonFacts = [];
+  while(n<array.length){
+  lennonFacts.push(array[n]+"!!!");
+   n++;
+  }
+  return lennonFacts;
+}
+
